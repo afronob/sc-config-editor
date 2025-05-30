f3ea7b2752008e916f8d1860500d213b5f26882c
Afronob <afronob@gmail.com>
2025-05-30 14:24:00 +0200

    change: iframe size

--- joy_iframe.js
+++ joy_iframe.js
@@ -5,12 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
             var url = this.getAttribute('data-url');
             var title = this.getAttribute('data-title');
             var cont = document.getElementById('joy_iframe_container');
+            // Adapter la taille de l'iframe à la moitié de la largeur et à la hauteur totale de la fenêtre, en gardant une marge
+            var margin = 20;
+            var width = Math.floor(window.innerWidth / 2) - margin * 2;
+            var height = window.innerHeight - margin * 2;
             cont.innerHTML = "<div style='text-align:right;background:#eee;padding:2px;'><button onclick=\"document.getElementById('joy_iframe_container').style.display='none'\">✖</button></div>"+
-                "<iframe src='"+url+"' title='"+title+"' style='width:600px;height:500px;border:0;'></iframe>";
+                "<iframe src='"+url+"' title='"+title+"' style='width:"+width+"px;height:"+height+"px;border:0;'></iframe>";
             cont.style.display = 'block';
-            cont.style.top = '20px';
+            cont.style.top = margin + 'px';
+            cont.style.right = margin + 'px';
             cont.style.left = 'auto';
-            cont.style.right = '20px';
         });
     });
 });
